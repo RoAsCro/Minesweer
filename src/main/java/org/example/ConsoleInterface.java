9511a963c6ed8a1fa5108cf67c21af5222c63d9a
Roland <rollycrompton@gmail.com>
2025-01-16 14:43:48 +0000

    Implemented getInt

--- src/main/java/org/example/ConsoleInterface.java
+++ src/main/java/org/example/ConsoleInterface.java
@@ -1,14 +1,34 @@
 package org.example;
 
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class ConsoleInterface implements UserInterface {
     @Override
     public int getInt(String prompt) {
-        return 0;
+        return getInt(prompt, -1);
     }
 
     @Override
     public int getInt(String prompt, int max) {
-        return 0;
+        while (true) {
+            display(prompt);
+            Scanner reader = new Scanner(System.in);
+            try {
+                int input = reader.nextInt();
+                if (max != -1 && input > max) {
+                    display("Please enter a number less than or equal to " + max + ".");
+                    continue;
+                }
+                if (input < 1) {
+                    display("Please enter a number greater than 0.");
+                    continue;
+                }
+                return input;
+            } catch (InputMismatchException e) {
+                display("Please enter a whole numeral.");
+            }
+        }
     }
 
     @Override
