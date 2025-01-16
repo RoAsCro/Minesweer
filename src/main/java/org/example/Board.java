871fc863c1f59ed5b1b648dc388bcfa20f7d8c35
Roland <rollycrompton@gmail.com>
2025-01-16 12:05:08 +0000

    getLocation checks for exceeding dimensions

--- src/main/java/org/example/Board.java
+++ src/main/java/org/example/Board.java
@@ -3,21 +3,28 @@ package org.example;
 public class Board {
     private final Location[][] locations;
     private final int mines;
+    private final int size;
 
     public Board(int size, int mines) {
         this.locations = new Location[size][size];
         // Todo DON'T EXCEED locations
         this.mines = mines;
+        this.size = size;
     }
 
     public boolean isMined(int x, int y) {
         return getLocation(x, y).mined;
     }
 
-    public print
-
-    private Location getLocation(int x, int y){
+    public Location getLocation(int x, int y){
+        if (checkExceeds(x) || checkExceeds(y)) {
+            return null;
+        }
         return this.locations[x][y];
+    }
+
+    private boolean checkExceeds(int coord) {
+        return coord < 0 || coord >= this.size;
     }
 
     public class Location{
