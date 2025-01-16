09c61b8485aa645521bfecdab43dcae5d726f18e
Roland <rollycrompton@gmail.com>
2025-01-16 13:16:46 +0000

    Begin implementing Game

---
+++ src/main/java/org/example/Game.java
@@ -0,0 +1,11 @@
+package org.example;
+
+public class Game {
+    private Board board;
+    private UserInterface userInterface;
+
+    public void makeMove(int x, int y){
+
+    }
+    public void display(){}
+}
