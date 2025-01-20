312c0cbfe86dd97c0735c327aa65ebbcd80eb520
Roland <rollycrompton@gmail.com>
2025-01-20 10:00:38 +0000

    implemented stack instead of recursion for revealing

--- src/main/java/org/example/Game.java
+++ src/main/java/org/example/Game.java
@@ -1,12 +1,13 @@
 package org.example;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 public class Game {
     private static final float MAX_MINE_PERCENTAGE = 0.25f;
     private static final int BOARD_MIN = 5;
-    private static final int BOARD_MAX = 25;
+    private static final int BOARD_MAX = 999;
     private static final int MINE_DEDUCTION = 9;
 
     private final UserInterface userInterface;
@@ -22,7 +23,8 @@ public class Game {
     public void setUp(){
         this.userInterface.display("Welcome to Minesweeper!");
         int size = 0;
-        String prompt = "Please enter the desired length of the board - boards are square and can be at most " + 25 +
+        String prompt = "Please enter the desired length of the board - boards are square and can be at most " +
+                BOARD_MAX +
                 " squares long.";
         String currentPrompt = prompt;
         while (size < BOARD_MIN) {
@@ -113,8 +115,8 @@ public class Game {
             this.userInterface.display("You hit a bomb!");
             return false;
         }
-        reveal(coord, new ArrayList<>());
-
+        revealLoop(coord);
+//        reveal(coord, new ArrayList<>());
         int size = this.board.getSize();
 
         if (revealedCount == size * size - this.board.getMines()) {
@@ -130,6 +132,40 @@ public class Game {
         this.userInterface.display("There are currently " + (size * size - this.revealedCount) +
                 " safe squares left to uncover.");
         this.userInterface.displayBoard(this.board, false);
+    }
+
+    private void revealLoop(Coordinate coord) {
+        LinkedList<Coordinate> used = new LinkedList<>();
+        LinkedList<Coordinate> stack = new LinkedList<>();
+        stack.add(coord);
+        used.add(coord);
+        while (!stack.isEmpty()) {
+            Coordinate current = stack.pop();
+            this.board.reveal(current);
+            this.revealedCount++;
+            if (this.board.getAdjacency(current) > 0) {
+                continue;
+            }
+            this.board.getIterator().iterateAdjacent(c -> {
+                        revealAdd(c, used, stack);
+                    },
+                    current);
+
+        }
+
+    }
+
+    private void revealAdd(Coordinate coord, List<Coordinate> used, List<Coordinate> stack) {
+        if (used.contains(coord) || stack.contains(coord)) {
+            return;
+        }
+        used.add(coord);
+        if (this.board.isMined(coord) || this.board.isRevealed(coord) || this.board.isFlagged(coord)) {
+            return;
+        }
+
+        used.add(coord);
+        stack.add(coord);
     }
 
     private void reveal(Coordinate coord, List<Coordinate> used) {
